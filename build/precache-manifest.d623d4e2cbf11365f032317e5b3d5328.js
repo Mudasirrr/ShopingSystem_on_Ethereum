9205b3dfaec465c63d85dbe0fa0f391412ad7723
Mudasir Hussain <40722738+Mudasirrr@users.noreply.github.com>
2019-08-13 16:51:39 -0700

    Add files via upload

---
+++ build/precache-manifest.d623d4e2cbf11365f032317e5b3d5328.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab79a397349cf1607640",
+    "url": "/ShopingSystem_on_Ethereum/static/js/runtime~main.ab79a397.js"
+  },
+  {
+    "revision": "b93d8c73c79b2bcdb09c",
+    "url": "/ShopingSystem_on_Ethereum/static/js/main.b93d8c73.chunk.js"
+  },
+  {
+    "revision": "6ca3994d372c9c927f67",
+    "url": "/ShopingSystem_on_Ethereum/static/js/1.6ca3994d.chunk.js"
+  },
+  {
+    "revision": "b93d8c73c79b2bcdb09c",
+    "url": "/ShopingSystem_on_Ethereum/static/css/main.b23419a3.chunk.css"
+  },
+  {
+    "revision": "6ca3994d372c9c927f67",
+    "url": "/ShopingSystem_on_Ethereum/static/css/1.d5bba1ad.chunk.css"
+  },
+  {
+    "revision": "2254d39e08267be4560ccee5d8a40b66",
+    "url": "/ShopingSystem_on_Ethereum/index.html"
+  }
+];
